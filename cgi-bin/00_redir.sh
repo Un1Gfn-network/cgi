61d35d38087877376fe8ad4358ed8ac0d8473afa
Darren Ng <darren19970810@gmail.com>
2020-05-18 22:43:46 +0800

    wildcard, show hidden

--- cgi-bin/00_redir.sh
+++ cgi-bin/00_redir.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 
 # D="$(dirname $PWD)"
+# TREE="tree -C -a -I .git* -H . --charset utf-8 -o "
+TREE="tree -C -a -I .git|.gitignore -H . --charset utf-8 -o "
 
 # https://stackoverflow.com/a/13864829
 if [ -n "${GATEWAY_INTERFACE+x}" ]; then
 
   source 00_rc
-  A
-  tree -H '.' --charset utf-8 -o ../index.html ..
-  echo "$?"
-  B
+  P
+  $TREE ../index.html ..
+  R="$?"
+  if [ "$R" -eq 0 ]; then
+    echo "Please go back to index.html and refresh it"
+  else
+    echo "Error! Please Check busybox httpd output."
+  fi
 
 else
 
   # https://stackoverflow.com/a/46383157
-  tree -H '.' --charset utf-8 -o index.html .
+  $TREE index.html .
 
 fi
