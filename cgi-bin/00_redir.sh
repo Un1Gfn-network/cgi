81670675be120758773e02009494018977249317
Darren Ng <darren19970810@gmail.com>
2020-05-18 21:29:28 +0800

    mime

--- cgi-bin/00_redir.sh
+++ cgi-bin/00_redir.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
 # D="$(dirname $PWD)"
-# D=/home/darren/http/httpd.sh
 
 # https://stackoverflow.com/a/13864829
 if [ -n "${GATEWAY_INTERFACE+x}" ]; then
+
   source 00_rc
   A
-  # echo sdf >garbage
-  # cat garbage
   tree -H '.' --charset utf-8 -o ../index.html ..
   echo "$?"
   B
+
 else
+
+  # https://stackoverflow.com/a/46383157
   tree -H '.' --charset utf-8 -o index.html .
+
 fi
